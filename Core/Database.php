23fb5ae99f3b1dd333302e0d7136a668b7e317ae
clonedkmh <quocanh10a2@gmail.com>
2023-02-18 00:03:43 +0700

    add models

---
+++ Core/Database.php
@@ -0,0 +1,18 @@
+<?php
+
+class Database
+{
+    const HOST = 'localhost';
+    const USERNAME = 'postgres';
+    const PASSWORD = '123456';
+    const DB_NAME = 'lession2';
+    const OPTIONS ='--client_encoding=UTF8';
+
+    private $connect;
+
+    public function connect(){
+        $this->connect = pg_connect(self::HOST, self::OPTIONS, self::USERNAME,
+            self::PASSWORD, self::DB_NAME);
+    }
+}
+?>
